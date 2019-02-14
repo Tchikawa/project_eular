4acbeffd9f2117dfc74b7d0eee236cd3593c2861
Tchikawa <k10d2019@gmail.com>
2019-02-14 12:00:51 +0900

    2問目

--- problem1.js
+++ problem1.js
@@ -2,7 +2,7 @@ var number = 1000;
 var answer = 0;
 
 for (var i = 1; i < number; i++) {
-  if (i % 3 === 0 || i % 5 === 0) anser += i;
+  if (i % 3 === 0 || i % 5 === 0) answer += i;
 }
 
 console.log(answer);
